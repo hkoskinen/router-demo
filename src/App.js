806e7f3bfe941d316735997340b09da729b1345f
Henri Koskinen <hkoskinen@protonmail.com>
2018-05-15 18:41:00 +0300

    Add few more routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
 
-        <Route path="/" component={Home} />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route component={Error} />
+        </Switch>
 
       </BrowserRouter>
     );
