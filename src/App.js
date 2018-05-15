3d192f761fc2904550e7d36efd9a5b76592d39d5
Henri Koskinen <hkoskinen@protonmail.com>
2018-05-15 18:47:32 +0300

    Add navigation

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import Error from './components/Error';
+import ErrorComponent from './components/Error';
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={Error} />
-        </Switch>
-
+        <div>
+          <Navigation  />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={ErrorComponent} />
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
